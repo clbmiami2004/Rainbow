4f08c6a7c7a1f98c100ca4876b4593e8f494c548
Christian Lorenzo <clbmiami2004@gmail.com>
2019-09-17 21:54:06 -0700

    Organized the project folder in categories.

---
+++ Rainbow/View Controllers/ColorsTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ColorsTableViewController.swift
+//  Rainbow
+//
+//  Created by Christian Lorenzo on 9/17/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import Foundation
