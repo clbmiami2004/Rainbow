f092bf8abeed81914513140834e2f340456c453e
Christian Lorenzo <clbmiami2004@gmail.com>
2019-09-17 23:18:03 -0700

    Added implementation for the DetailViewController

--- Rainbow/View Controllers/ColorsDetailViewController.swift
+++ Rainbow/View Controllers/ColorsDetailViewController.swift
@@ -8,6 +8,24 @@
 
 import UIKit
 
+//This screen is used to show the name of the color being shown and the Actual Color as the background.
+
 class ColorDetailViewController: UIViewController {
+    
+    var cellColor: Color?  // This need to place this variable everytime we want to show a different color.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateViews()
+    }
+    
+    func updateViews() {
+        
+        guard let cellColor = cellColor else {return}
+        
+        title = cellColor.name
+        view.backgroundColor = cellColor.color
+    }
     
 }
